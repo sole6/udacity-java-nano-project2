e5c754a94deab4afb20dbb2f881cc526a75092a2
Sololia <getsoli2003@gmail.com>
2020-11-19 21:49:57 +0100

    project2 final

---
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -0,0 +1,33 @@
+package com.udacity.pricing;
+
+import com.udacity.pricing.api.PricingController;
+import com.udacity.pricing.domain.price.Price;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PricingServiceApplicationTests {
+	@LocalServerPort
+	private int port;
+	@Autowired
+	private TestRestTemplate restTemplate;
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void getPricing() throws Exception {
+		assertThat(this.restTemplate.getForEntity("http://localhost:" + port + "/services/price",
+				Price.class).equals(HttpStatus.OK));
+	}
+
+}
